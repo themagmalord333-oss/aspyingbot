c26f1cb021339fb42ddcd85a250cb1c63a679dc7
themagmalord333-oss <the.magma.lord.333@gmail.com>
2026-07-30 19:36:20 +0530

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, BigInteger, Float
+from sqlalchemy import Column, Integer, String, BigInteger, Float, DateTime
 from sqlalchemy.orm import declarative_base
+from datetime import datetime
 
 Base = declarative_base()
 
@@ -8,10 +9,16 @@ class Subscription(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(BigInteger, nullable=False)
     username = Column(String, nullable=False)
-    alert_price = Column(Float, nullable=True)
 
 class TrackedWallet(Base):
     __tablename__ = "tracked_wallets"
     id = Column(Integer, primary_key=True)
     user_id = Column(BigInteger, nullable=False)
     address = Column(String, nullable=False)
+
+class Reminder(Base):
+    __tablename__ = "reminders"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, nullable=False)
+    item_name = Column(String, nullable=False)
+    remind_at = Column(DateTime, default=datetime.utcnow)
